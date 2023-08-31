98a57bee9c68c06d1b301ed0629d692ecc203eca
gomex500 <gomezfreddy886@gmail.com>
2023-08-31 17:47:25 -0600

    trabanjando en responsive del jumbotron

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,11 +8,11 @@ const Home = () =>{
         <div className="seccion">
             <div className="jumbo animate__animated animate__fadeIn">
                 <div className="row">
-                    <div className="col-12 col-md-8 col-lg-8 cont">
+                    <div className="col-12 col-sm-12 col-md-7 col-lg-8 cont">
                         <p>"Optimiza tu proceso de planificación y presupuesto en construcción con <b>CubeCheck</b>. Obtén presupuestos detallados y precisos en minutos, sin errores y estimaciones aproximadas. Visualiza tu proyecto en 3D y genera presupuestos profesionales en PDF. Ahorra tiempo y esfuerzo, toma decisiones informadas y destaca en tus proyectos de construcción con <b>CubeCheck</b>, la herramienta imprescindible para el éxito."</p>
                         <p className="eslogan">"CubeCheck: La clave para construir de manera inteligente y eficiente."</p>
                     </div>
-                    <div className="col-12 col-md-4 col-lg-4 cont-2">
+                    <div className="col-12 col-sm-12 col-md-5 col-lg-4 cont-2">
                         <img src={logo} alt="logo"/>
                         <Btn1
                             text={"Iniciar ahora"}
