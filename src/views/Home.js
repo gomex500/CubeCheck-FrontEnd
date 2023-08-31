74d4f44c2d01e0111f7f5dfa9af4c44318a6e76d
gomex500 <gomezfreddy886@gmail.com>
2023-08-31 00:15:06 -0600

    trabajando en el jumbotrom

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,11 @@ const Home = () =>{
     return(
         <div className="seccion">
             <div className="jumbo">
-                
+                <div className="row">
+                    <div className="col-12">
+                        <p>"Optimiza tu proceso de planificación y presupuesto en construcción con CubeCheck. Obtén presupuestos detallados y precisos en minutos, sin errores ni estimaciones aproximadas. Visualiza tu proyecto en 3D y genera presupuestos profesionales en PDF. Ahorra tiempo y esfuerzo, toma decisiones informadas y destaca en tus proyectos de construcción con CubeCheck, la herramienta imprescindible para el éxito."</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
