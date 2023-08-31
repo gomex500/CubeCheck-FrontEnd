5715a7cbf7ceb3fc23b820636f17c3225e588f16
gomex500 <gomezfreddy886@gmail.com>
2023-08-30 21:08:04 -0600

    continuacion del navbar

--- src/views/partials/Navbar.js
+++ src/views/partials/Navbar.js
@@ -1,22 +1,44 @@
-import React, {useState} from "react";
+import React from "react";
 import logo from '../../img/logo.png'
 import '../../css/navbar.css';
 
 const Navbar = () =>{
-
-    const [nav, setNav] = useState(true);
-    const handleNavClick = () => {
-        setNav(!nav);
-      };
     return(
         <div>
-            <nav className="navbaraux">
-                <button onClick={handleNavClick} className="logo">
-                    <img src={logo} alt='logo'/>
-                    <span className="nav-item">CubeCheck</span>
-                </button>
+            <nav className="navbar navbar-expand-lg nav1">
+                <div className="container-fluid">
+                    <a className= "logo navbar-brand" href="/" >
+                        <img src={logo} alt="logo"/>
+                        <span className="nav-item">CubeCheck</span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="/"><i class="fas fa-home"></i> Inico</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/beneficios"><i class="fas fa-user"></i> User</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/proceso"><i class="bi bi-gear-fill"></i> Proceso</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Juego"><i class="bi bi-controller"></i> Juego</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/ayuda"> <i class="bi bi-info-circle-fill"></i> Ayuda</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#footer"><i class="bi bi-telephone-fill"></i> Contactos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            <nav className={nav ? "navbarr animate__animated animate__fadeInLeftBig" : "navbarr1"}>
+            <nav className="navbarr">
             <ul>
                 <li>
                     <a href="/" className="logo">
