2d4e0adb1f9b988b8aff75e9ffef786ddfbffb6f
gomex500 <gomezfreddy886@gmail.com>
2023-08-31 21:39:10 -0600

    terminacion del chatbot prototipo

--- src/components/BtnChat.js
+++ src/components/BtnChat.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
+import axios from "axios";
 import '../css/chatBot.css'
 import Btn2 from "./Btn2";
 import Input from "./Input";
@@ -6,12 +7,46 @@ import Input from "./Input";
 
 
 const BtnChat = () =>{
-
     const [visible, setVisible] = useState(false);
+    const [input, setInput] = useState('');
+    const [conversation, setConversation] = useState([]);
+    const chatboxRef = useRef(null);
 
     const btnVisible = () =>{
         setVisible(!visible);
     }
+
+    const conversacion = async (e) =>{
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://hydrobot.onrender.com/chat', {input: input});
+            const Usuario = {
+              user: 'Tu',
+              message: input,
+              type: 'user'
+            };
+      
+            const bot = {
+              user: 'CubeBot',
+              message: response.data.response,
+              type: 'bot'
+            };
+            setConversation((prevConversation) => [
+              ...prevConversation,
+              Usuario,
+              bot
+            ]);
+      
+            setInput('');
+      
+          } catch (error) {
+          }
+      }
+
+      useEffect(() => {
+        const chatboxElement = chatboxRef.current;
+        chatboxElement.scrollTop = chatboxElement.scrollHeight;
+      }, [conversation]);
 
     return(
         <div className="con-chatbot">
@@ -20,17 +55,21 @@ const BtnChat = () =>{
                     <i class="fa-solid fa-robot"></i>
                     <span>CubeBot</span>
                 </div>
-                <div className="chatbody">
-                    
+                <div ref={chatboxRef} className="chatbody">
+                    {conversation.map((message, index) => (
+                    <div key={index} className={`message ${message.type === 'user' ? 'user' : 'bot animate__animated animate__bounceInLeft'}`}>
+                        <strong>{message.user}:  </strong>{message.message}
+                    </div>
+                    ))}
                 </div>
                 <div className="chatfooter">
-                    <form>
+                    <form onSubmit={conversacion}>
                         <Input
                             tp={"text"}
                             cls={"form-control inputChat"}
-                            // val={""}
+                            val={input}
                             ph={"Ingrese su pregunta"}
-                            fuc={console.log("")}
+                            fuc={(e) => setInput(e.target.value)}
                         />
                         <Btn2
                             tp={"submit"}
