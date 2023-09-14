fc3c245cf37bacb12278b07d0cd6f670f8152174
gomex500 <gomezfreddy886@gmail.com>
2023-09-14 17:23:09 -0600

    se cmabio el deseno de registro ya q no e convencia

--- src/views/Sigup.js
+++ src/views/Sigup.js
@@ -3,117 +3,86 @@ import '../css/sigup.css';
 import Btn1 from '../components/Btn1';
 import Input from '../components/Input';
 import Btn2 from "../components/Btn2";
+import logo from '../img/logo.png';
 
 const Sigup = () =>{
     return(
         <div className="seccion">
             <div className="con-sigup animate__animated animate__fadeInDown">
                 <div className="row">
-                    <div className="cont1">
-                        <p>Registro de Usuario</p>
+                    <div className="sigup-img">
+                        <center>
+                            <img src={logo} alt="logo"/>
+                            <h2>CubeCheck</h2>
+                        </center>
                     </div>
-                    <div className="cont2"></div>
-                </div>
-                <div className="form-sigup">
-                    <form className="row form">
-                        <div className="form1">
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su nombre"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su apellido"}
-                            />
-                            <Input
-                                tp={"email"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su @gmail"}
-                            />
-                            <Input
-                                tp={"password"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su contrasena"}
-                            />
-                            <Input
-                                tp={"password"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese contrasena nuevamente"}
-                            />
-                        </div>
-                        <div className="form2">
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su nombre completo"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su tarjeta"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su codigo"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input2"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese mes"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input2"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su ano"}
-                            />
-                            <Input
-                                tp={"text"}
-                                cls={"form-control input"}
-                                fuc={console.log("")}
-                                // val={}
-                                ph={"Ingrese su region"}
-                            />
-                        </div>
-                        <Btn2
-                            tp={"sumit"}
-                            cls={"btn1"}
-                            text={"Registrar"}
-                        />
-                    </form>
-                </div>
-                <div className="row">
-                    <div className="cont3"></div>
-                    <div className="cont4">
-                        <p>Ya tienes cuenta
+                    <div className="sigup-form">
+                        <center>
+                            <h2>Registro</h2>
+                            <form className="form">
+                                <Input
+                                    tp={"text"}
+                                    cls={"form-control input1"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Nombre"}
+                                />
+                                <Input
+                                    tp={"text"}
+                                    cls={"form-control input1"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Apellido"}
+                                />
+                                <Input
+                                    tp={"number"}
+                                    cls={"form-control input2"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Edad"}
+                                />
+                                <Input
+                                    tp={"tel"}
+                                    cls={"form-control input3"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Telefono"}
+                                />
+                                <Input
+                                    tp={"email"}
+                                    cls={"form-control input"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Correo"}
+                                />
+                                <Input
+                                    tp={"password"}
+                                    cls={"form-control input"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Password"}
+                                />
+                                <Input
+                                    tp={"password"}
+                                    cls={"form-control input"}
+                                    // val={""}
+                                    fuc={console.log('')}
+                                    ph={"Password nuevamente"}
+                                />
+                                <Btn2
+                                    tp={"sumit"}
+                                    cls={"btn3"}
+                                    text={"Iniciar"}
+                                />
+                            </form>
+                            <p>ya tienes cuenta
                                 <Btn1
-                                    text={"Ingresa Aqui"}
+                                    text={"Ingresa aqui"}
                                     cls={"btn2"}
                                     url={"/login"}
                                 />
                             </p>
+                        </center>
                     </div>
                 </div>
             </div>
